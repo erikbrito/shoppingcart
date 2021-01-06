7b7aa1150531a1396ae3283f708215648c93272d
erikbrito <erikbrito01@gmail.com>
2021-01-06 16:19:58 -0300

    stylized navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Routes from './Routes'
 
+import './styles.scss'
+
 function App() {
   return (
     <Routes />
--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { MdShoppingCart } from 'react-icons/md';
+import { AiFillShopping } from 'react-icons/ai';
+import './Navbar.scss'
 
 const Navbar = () => {
   const Cart = useSelector(state => state.cartReducer)
 
   return (
-    <div>
-      <div>
-        <Link to="/checkout">checkout</Link>
-      </div>
-      <div>
-        <h1> {Cart.length} </h1>
-      </div>
-    </div>
+    <header id="main-header">
+      <Link id="logo" to="/">
+        <AiFillShopping size={36} color="#FFF" />
+        <strong>Logo</strong>
+      </Link>
+
+      <Link id="link" to="/checkout">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>
+            {Cart.length} {Cart.length > 1 ? 'itens' : 'item'}
+          </span>
+        </div>
+        <MdShoppingCart size={36} color="#FFF" />
+      </Link>
+    </header>
   )
 }
 
